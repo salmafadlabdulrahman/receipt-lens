ed1c2d5138cab3230a4372e151a1e343ae95876c
Salma Fadl <salmafadlabdulrahman@gmail.com>
2025-11-11 20:17:44 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-indigo-500 text-4xl">Hello world</h1>
+      <Button variant="contained" color="primary">
+        MUI Button
+      </Button>
+    </>
+  );
+}
+
+export default App;
