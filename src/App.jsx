1a679e21d30beb0a4d80d87673b535327699758e
Salma Fadl <salmafadlabdulrahman@gmail.com>
2025-11-14 11:24:52 +0200

    build contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import "./index.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Contact from "./pages/Contact";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <Footer />
+    <div className="">
+      <Router>
+        <Navbar />
+        <main className="grow pt-0">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </Router>
+
+      <footer className="">
+        <Footer />
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+//flex flex-col min-h-screen
