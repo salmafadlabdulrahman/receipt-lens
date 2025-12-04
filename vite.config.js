647f867735c83cf518c86b65cde383af00379a46
Mostafa Ahmed <mostafaa7med226@gmail.com>
2025-12-04 21:25:56 +0200

    dashboard

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), // اختصار لمسار src
-    },
-  },
-  server: {
-    host: "::",
-    port: 5173, // تقدر تغيّر البورت لو تحب
-  },
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from "url";
